a4cc00af1f427f36c7feb1ad71571a64da2945fd
lovro <hodak.lovro@gmail.com>
2021-01-11 18:33:30 +0100

    router.patch doesnt work

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -1,14 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const itemModel = require('../models/item.model')
+const express = require("express");
+const router = express.Router();
+const itemModel = require("../models/item.model");
 
 //that means your browser route is: localhost:5050/api/items
-router.get('/items', (req, res) => {
-    itemModel.find()
-        .then((stuff) => {
-            //console.log(stuff)
-            res.status(200).json(stuff)
-        })
-})
+router.get("/items", (req, res) => {
+  itemModel.find().then((stuff) => {
+    console.log('stuff is', stuff);
+    res.status(200).json(stuff);
+  });
+});
 
-module.exports = router
+router.get("/items/:itemId", (req, res) => {
+  let id = req.params.itemId;
+  const {nrOfItems} = req.body;
+  itemModel
+    .findById(id)
+    .then((response) => {
+      res.status(200).json(response);
+      console.log("req.params: ", req.params); /* req.params:  { itemId: '5ffc4407039cd02960564f35' } */
+      console.log("response.nrOfItems: ", response.nrOfItems);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
+router.patch("/items/:itemId", (req, res) => {
+  let id = req.params.id;
+  itemModel
+    .findByIdAndUpdate(id, { $set: { nrOfItems: req.body.nrOfItems } })
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: err,
+      });
+    });
+});
+
+module.exports = router;
